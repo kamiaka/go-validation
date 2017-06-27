a71fa77de2f8fca756bb842ccb6a053126b715e2
Shinya Kamiaka <shinya@kamiaka.net>
2017-06-27 23:55:10 +0900

    Add match rule

---
+++ match.go
@@ -0,0 +1,46 @@
+package validation
+
+import (
+	"fmt"
+	"regexp"
+)
+
+type matchRule struct {
+	re     *regexp.Regexp
+	format string
+}
+
+func Match(re *regexp.Regexp) BuiltInFieldRule {
+	return &matchRule{
+		re:     re,
+		format: "%[1]v must be in a valid format",
+	}
+}
+
+func (r *matchRule) Format(format string) BuiltInFieldRule {
+	return &matchRule{
+		re:     r.re,
+		format: format,
+	}
+}
+
+func (r *matchRule) Apply(fi FieldInfo) error {
+	if fi.IsEmpty() {
+		return nil
+	}
+
+	isStr, str, isBytes, bs := StringOrBytes(fi.Value())
+	if isStr {
+		if r.re.MatchString(str) {
+			return nil
+		}
+	} else if isBytes {
+		if r.re.Match(bs) {
+			return nil
+		}
+	} else {
+		return fmt.Errorf("cannot convert %v to string or bytes", fi.Value().Type())
+	}
+
+	return newFieldError(fi, r.format, fi.Label())
+}
